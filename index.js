1353a6289da75aa1c251285980e00afa81e42626
Sunit Corneleous <sunit.corneleous25@gmail.com>
2022-11-16 14:29:00 +0600

    mongodb added

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// mongodb config
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jjvalws.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  //   client.close();
+  console.log("***** database successfully connected *****");
+});
+
 //home route
 app.get("/", (req, res) => {
   res.send("<h1>Doctors portal server is running 🚀</h1>");
